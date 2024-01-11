d656d153c1745416ec955411ec2b5cfb6bcc4f73
maleficush <maleficush@gmail.com>
2024-01-11 16:25:57 +0900

    - first commit

---
+++ _1_rabbitmq_producer/src/test/java/com/example/_1_rabbitmq_producer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._1_rabbitmq_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
